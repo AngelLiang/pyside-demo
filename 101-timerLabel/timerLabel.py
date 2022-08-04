48e8aa6db1ad5e3f24722d3f6314980aa6c23fd7
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2022-08-04 11:26:22 +0800

    fix: 修复 timerLabel 组件的 bug

--- 101-timerLabel/timerLabel.py
+++ 101-timerLabel/timerLabel.py
@@ -1,11 +1,32 @@
 import sys
+import datetime
 
 from PySide6.QtCore import Signal
 from PySide6.QtCore import QTime, QTimer
 from PySide6.QtWidgets import QApplication, QWidget, QGridLayout
 from PySide6 import QtCore, QtGui
-from PySide6.QtWidgets import QLabel
+from PySide6.QtWidgets import QLabel, QPushButton
 
+DEBUG = True
+
+import sys
+import time
+import threading
+
+
+loglock = threading.Lock()
+
+def log(s, *a):
+	
+    """打印日志到标准输出"""
+    if DEBUG:
+        loglock.acquire()
+        try:
+            print('%s:%s' % (time.ctime(), (s % a)))
+            sys.stdout.flush()
+        finally:
+            loglock.release()
+            
 
 class TimerLabel(QLabel):
     doubleClicked = Signal()
@@ -36,9 +57,11 @@ class TimerLabel(QLabel):
         self.__format = 'hh:mm:ss'
 
         self.__timer = QTimer(self)
+        self.__timer.setSingleShot(False)
+        self.__timer.timeout.connect(self.__timerTicking)
         self.__timer_interval = -1
 
-        self.is_after_stop_reset_time = False
+        self.__is_after_stop_reset_time = False
 
         # init end time (00:00:00 by default)
         self.setEndHMS()
@@ -47,10 +70,10 @@ class TimerLabel(QLabel):
         self.setStartHMS()
 
     def setAfterStopResetTime(self):
-        self.is_after_stop_reset_time = True
+        self.__is_after_stop_reset_time = True
 
     def resetAfterStopResetTime(self):
-        self.is_after_stop_reset_time = False
+        self.__is_after_stop_reset_time = False
 
     def setTimerReverse(self, f: bool):
         if f:
@@ -59,15 +82,13 @@ class TimerLabel(QLabel):
             self.__timer_interval = 1
 
     def setStartHMS(self):
-        self.__startTime.setHMS(
-            self.__start_hour, self.__start_min, self.__start_sec)
+        self.__startTime.setHMS(self.__start_hour, self.__start_min, self.__start_sec)
         time_left_text = self.__startTime.toString(self.__format)
         self.setText(time_left_text)
 
     def setEndHMS(self):
         self.__endTime.setHMS(self.__end_hour, self.__end_min, self.__end_sec)
-        self.__end_text_time = self.__endTime.addSecs(
-            -1).toString(self.__format)
+        self.__end_text_time = self.__endTime.addSecs(-1).toString(self.__format)
 
     def setStartHour(self, h: int):
         self.__start_hour = h
@@ -98,9 +119,8 @@ class TimerLabel(QLabel):
         self.prepared.emit()
 
     def start(self):
-        self.__timer.timeout.connect(self.__timerTicking)
         self.__timer.singleShot(self.__startTime.msec(), self.__prepareToTimer)
-        # update the timer every millisecond
+        # update the timer every second
         self.__timer.start(1)
         self.started.emit()
 
@@ -110,7 +130,9 @@ class TimerLabel(QLabel):
     def __timerTicking(self):
         try:
             self.__startTime = self.__startTime.addMSecs(self.__timer_interval)
+            # self.__startTime = self.__startTime.addSecs(self.__timer_interval)
             time_left_text = self.__startTime.toString(self.__format)
+            # log(time_left_text)
             if self.__isTimesUp(time_left_text):
                 self.stop()
             else:
@@ -129,8 +151,7 @@ class TimerLabel(QLabel):
         self.restarted.emit()
 
     def __resetStartTime(self):
-        self.__startTime = QTime(
-            self.__start_hour, self.__start_min, self.__start_sec)
+        self.__startTime = QTime(self.__start_hour, self.__start_min, self.__start_sec)
         self.setText(self.__startTime.toString(self.__format))
 
     def __resetTimer(self):
@@ -147,8 +168,10 @@ class TimerLabel(QLabel):
         self.refreshed.emit()
 
     def stop(self):
+        if DEBUG:
+            log('stop')
         try:
-            if self.is_after_stop_reset_time:
+            if self.__is_after_stop_reset_time:
                 self.__resetTimer()
             self.__timer.stop()
             self.stopped.emit()
@@ -175,10 +198,20 @@ if __name__ == "__main__":
     lbl.setFont(QtGui.QFont("Roman times", 28, QtGui.QFont.Bold))
     lbl.setStartHour(0)
     lbl.setStartMinute(0)
-    lbl.setStartSecond(5)
+    lbl.setStartSecond(10)
+
+
+    def start():
+        log('start')
+        lbl.setStartMinute(0)
+        lbl.setStartSecond(10)
+        lbl.start()
 
     lay = QGridLayout()
     lay.addWidget(lbl)
+    startButton = QPushButton("start")
+    startButton.clicked.connect(start)
+    lay.addWidget(startButton)
 
     widget = QWidget()
     widget.setLayout(lay)
